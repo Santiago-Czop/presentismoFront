ee72f3c95a9c02f88685cb00b660293ed434fa9e
Santiago Nicolás Czop <czopsantiago@gmail.com>
2022-08-05 17:48:51 -0300

    [ADD] - Attempt at release version

--- android/app/src/main/kotlin/com/sczop/presente/MainActivity.kt
+++ android/app/src/main/kotlin/com/sczop/presente/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.front
+package com.sczop.presente
 
 import io.flutter.embedding.android.FlutterActivity
 
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:front/server.dart';
+import 'package:presente/server.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
